7bbcffb1d2eaa0d047921de573d701a9604d6cd3
Andrej Bulavcak <68511471+AveApollo@users.noreply.github.com>
2022-01-11 20:25:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const Gun = require('gun')
+var port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
+var express = require('express');
+var Gun     = require('..');
+require('../axe');
 
-const app = express()
-const port = 8000
-app.use(Gun.serve)
+var app    = express();
+app.use(Gun.serve);
+app.use(express.static(__dirname));
 
-const server = app.listen(port, () => {
-    console.log("Listening at: http://localhost://" + port)
-})
+var server = app.listen(port);
+var gun = Gun({ file: 'data', web: server });
 
-Gun({web: server})
+global.Gun = Gun; /// make global to `node --inspect` - debug only
+global.gun = gun; /// make global to `node --inspect` - debug only
+
+console.log('Server started on port ' + port + ' with /gun');
