6f7d7fdea15fa5636ac16ba6db3b46e4d622e433
Andrej Bulavcak <68511471+AveApollo@users.noreply.github.com>
2022-01-12 09:30:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const Gun = require('gun');
 const SEA = require("gun/sea");
+const cors = require('cors')
 
 const port = (process.env.PORT || 8080);
 const host = '0.0.0.0';
@@ -13,29 +14,38 @@ const server = app.listen(port, host);
 
 console.log(`server listening on http://${host}:${port}`);
 
-function logIn(msg){
-  console.log(`in msg:${JSON.stringify(msg)}.........`);
+
+const corsOptions = {
+    origin: 'https://yourdomain.com',
 }
 
-function logOut(msg){
-  console.log(`out msg:${JSON.stringify(msg)}.........`);
+app.get('/', cors(corsOptions), (req, res, next) => {
+    console.log("not allowed")
+})
+
+function logIn(msg) {
+    console.log(`in msg:${JSON.stringify(msg)}.........`);
+}
+
+function logOut(msg) {
+    console.log(`out msg:${JSON.stringify(msg)}.........`);
 }
 
 var gun = Gun({
-  web: server,
-  localStorage: false,
-  radisk: false
+    web: server,
+    localStorage: true,
+    radisk: true
 });
 
 gun._.on('in', logIn);
 gun._.on('out', logOut);
 
 function logPeers() {
-  console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
+    console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
 }
 
 function logData() {
-  console.log(`In Memory: ${JSON.stringify(gun._.graph)}`);
+    console.log(`In Memory: ${JSON.stringify(gun._.graph)}`);
 }
 
 setInterval(logPeers, 5000); //Log peer list every 5 secs
@@ -46,7 +56,7 @@ const view = path.join(__dirname, 'view/main.html');
 
 app.use(express.static('view'));
 app.get('*', function(_, res) {
-  res.sendFile(view);
+    res.sendFile(view);
 });
 
 
